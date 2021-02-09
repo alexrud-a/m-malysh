80002290bd783b0c98954df97ca4cb8947f6ce3d
AleXandra Rudivickaya <rag@one-touch.ru>
2021-02-09 23:58:50 +0300

    install vue

--- wp-content/themes/malysh/index.php
+++ wp-content/themes/malysh/index.php
@@ -18,8 +18,8 @@ get_header();
 <div id="app">
 </div>
 
-    <script src="/wp-content/themes/malysh/malysh/dist/js/chunk-vendors.js"></script>
-    <script src="/wp-content/themes/malysh/malysh/dist/js/index.js"></script>
+    <script src="/wp-content/themes/malysh/dist/js/chunk-vendors.js"></script>
+    <script src="/wp-content/themes/malysh/dist/js/index.js"></script>
 
 
 	<main id="primary" class="site-main">
