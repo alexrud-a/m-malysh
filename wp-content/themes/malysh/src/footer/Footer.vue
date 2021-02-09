8488ccf71c9e2622e25c1191ecd26169c25ec8ef
AleXandra Rudivickaya <rag@one-touch.ru>
2021-02-10 00:37:06 +0300

    add header, footer

--- wp-content/themes/malysh/src/footer/Footer.vue
+++ wp-content/themes/malysh/src/footer/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer id="footer">
-
+    <h1>FOOTER</h1>
   </footer>
 </template>
 
--- wp-content/themes/malysh/src/footer/main.js
+++ wp-content/themes/malysh/src/footer/main.js
@@ -6,4 +6,4 @@ Vue.config.productionTip = false
 
 new Vue({
     render: h => h(Footer),
-}).$mount('#header')
+}).$mount('#footer')
